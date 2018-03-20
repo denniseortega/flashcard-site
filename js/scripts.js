1be68cb0e0ff50b9907ad4b0ea09824e23c9dbb1
Dennise Ortega and Eva Antipina <dennise@>
2018-03-20 15:09:56 -0700

    Finish flash cards, changing adding more classes and changing some classes to id to make flashcards work indipendently

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,48 @@
 $(document).ready(function(){
-  $(".clickable").click(function() {
-    $(".front").toggle();
+  $(".clickable1").click(function() {
+    $("#front1").toggle();
   });
-  $(".clickable").click(function() {
-    $(".back").toggle();
+  $(".clickable1").click(function() {
+    $("#back1").toggle();
+  });
+
+
+  $(".clickable2").click(function() {
+    $("#front2").toggle();
+  });
+  $(".clickable2").click(function() {
+    $("#back2").toggle();
+  });
+
+
+  $(".clickable3").click(function() {
+    $("#front3").toggle();
+  });
+  $(".clickable3").click(function() {
+    $("#back3").toggle();
+  });
+
+
+  $(".clickable4").click(function() {
+    $("#front4").toggle();
+  });
+  $(".clickable4").click(function() {
+    $("#back4").toggle();
+  });
+
+
+  $(".clickable5").click(function() {
+    $("#front5").toggle();
+  });
+  $(".clickable5").click(function() {
+    $("#back5").toggle();
+  });
+
+
+  $(".clickable6").click(function() {
+    $("#front6").toggle();
+  });
+  $(".clickable6").click(function() {
+    $("#back6").toggle();
   });
   });
